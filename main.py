81fa1ed942dba96edbb0c338e4ca7a829852df5a
brenopavanelli <brenopavanelli3@gmail.com>
2025-10-08 22:18:35 -0300

    feat: data sorted by employee name

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def load_data(pasta, arquivo):
 def delete_columns(df):
     columns_to_remove = ['ParCalc', 'DescrParCalc', 'Qtde_Total', 'Valor_Total']
 
-    clean_df = df.drop(columns=columns_to_remove)
+    new_df = df.drop(columns=columns_to_remove)
 
     print(f"Columns '{', '.join(columns_to_remove)}' successfully removed!")
 
-    return clean_df
+    return new_df
 
 def columns_rename(df):
     map = {
@@ -38,11 +38,16 @@ def columns_rename(df):
 
     return new_df
 
+def order_data(df):
+    new_df = df.sort_values(by='NOME DO FUNCIONÁRIO')
+    print('DataFrame orderd by "NOME DO FUNCIONÁRIO"')
 
+    return new_df
 
 if __name__ == "__main__":
     dataframe = load_data('dados_entrada', 'entrada.csv')
     cleaned_df = delete_columns(dataframe)
-    df_renamed = columns_rename(cleaned_df)
+    renamed_df = columns_rename(cleaned_df)
+    ordered_df = order_data(renamed_df)
 
-    print(df_renamed)
+    print(ordered_df)
